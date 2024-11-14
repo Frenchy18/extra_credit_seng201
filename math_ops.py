d2eb910ad7511930ee2ecedd475317540f2a0e82
Chris Groves <chris.grover9292@gmail.com>
2024-11-14 14:43:16 -0500

    added addition feature

--- math_ops.py
+++ math_ops.py
@@ -1,11 +1,17 @@
 # math_operations.py
 
 def divide_numbers(a, b):
-    """Divids two numbers and returns the result."""
+    """Divides two numbers and returns the result."""
     return a / b # Potential division by zero error
+
+def add_numbers(a, b):
+    """Adds two numbers and returns the result."""
+    return a + b # Potential coolness error
 
 if __name__ == "__main__":
     x = 10
     y = 0
     result = divide_numbers(x, y)
     print(f"The result of division is: {result}")
+    result = add_numbers(x, y)
+    print(f"The result of addition is: {result}")
