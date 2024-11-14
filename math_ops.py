e7f036ebbb6d60acd11e7996d28bb18f15edb6ac
Chris Groves <chris.grover9292@gmail.com>
2024-11-14 14:38:06 -0500

    Added exception handling for diving by 0

--- math_ops.py
+++ math_ops.py
@@ -2,6 +2,9 @@
 
 def divide_numbers(a, b):
     """Divids two numbers and returns the result."""
+    if b == 0:
+        raise ValueError("Sorry, you cannot divide by 0")
+    
     return a / b # Potential division by zero error
 
 if __name__ == "__main__":
